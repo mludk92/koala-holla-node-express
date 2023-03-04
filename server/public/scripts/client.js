794cffe25e9b7c723fe527dcdfd0c70af5ca190f
Mitch Ludkey <mludk92@hotmail.com>
2023-03-04 09:00:33 -0600

    can right to table and post set up on server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,29 @@
 console.log( 'js' );
 
+let nameIn = document.querySelector('#nameIn').value
+let ageIn = document.querySelector('#ageIn').value
+let genderIn = document.querySelector('#genderIn').value
+let rFFIn = document.querySelector('#readyForTransferIn').value
+let notesIn = document.querySelector('#notesIn').value
+
+
+function submitKoala(event){
+  console.log(nameIn,ageIn,genderIn,rFFIn,notesIn, 'input fields')
+  writeTable()
+}
+
+
+function writeTable(){document.querySelector('#viewKoalas').innerHTML += 
+`<tr>
+  <td>${nameIn}</td>
+  <td>${ageIn}</td>
+  <td>${genderIn}</td>
+  <td>${rFFIn}</td>
+  <td>${notesIn}</td>
+</tr>`}
+
+
+
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
@@ -9,7 +33,6 @@ function getKoalas(){
 function saveKoala(){
   console.log( 'in saveKoala' );
   // axios call to server to get koalas
- 
 }
 
 getKoalas();
--- server/server.js
+++ server/server.js
@@ -9,6 +9,32 @@ app.use(express.static('server/public'));
 // ROUTES
 app.use('/koalas', koalaRouter);
 
+let koalaTable = 
+[{id:1,
+name:'name',
+gender:'gender',
+age:1,
+readyToTransfer:'rtt',
+notes:'notes',
+},]
+
+// POST request save user input
+get.post('/koalas',(req,res) => {
+  console.log('Post request made for /quotes')
+  //any data we send from the client is available 
+  //as a property of req.body
+  console.log(req.body)
+  let koalaToAdd = req.body
+  koalaTable.push(koalaToAdd)
+  res.sendStatus(201)//success
+})
+
+
+
+
+
+
+
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
