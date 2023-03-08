3f4b978bd8489ae731563e599d25f79a9cd377af
Mitch Ludkey <mitchell.ludkey@blueearthcountymn.gov>
2023-03-08 13:13:33 -0600

    moved crud from server to router.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,18 +1,57 @@
 const express = require('express');
-const koalaRouter = express.Router();
+const router = express.Router();
 
 // DB CONNECTION
 
 
 // GET
-
+//gets response of input fields submitted by user
+router.get('/',(req,res) => {
+    console.log('GET request made for /quotes')
+    //to send back the list of quotes
+    console.log(koalaTable, 'the get is here ')
+    res.send(koalaTable)
+  })
+  
 
 // POST
+//creates unique id for koalas and sends back all informaiton in koalaTable
 
+let i = 1 //starting indexing at 1 
+let koalaTable = []
+router.post('/',(req,res) => {
+    console.log('Post request made for /quotes')
+    //any data we send from the client is available 
+    //as a property of req.body
+    //console.log(req.body)
+    let koalaToAdd = req.body
+    //console.log(koalaToAdd[0].name,'test for name ')
+    koalaTable.push({
+      id:Number(i)+1,
+      name:koalaToAdd[0].name,
+      gender:koalaToAdd[0].gender,
+      age:koalaToAdd[0].age,
+      readyToTransfer:koalaToAdd[0].readyToTransfer,
+      notes:koalaToAdd[0].notes,
+      })
+    i++
+
+    console.log(koalaTable, 'the table is this')
+    res.sendStatus(201)//success
+  })
 
 // PUT
 
 
 // DELETE
+//removes entry from koalatable where the index of the entry is referenced on click. 
+router.delete('/:id',(req,res)=>{
+    //What are we deleting ???
+    console.log(req.params.id)
+    const deleteIndex = Number(req.params.id)  //Similar to req.body but subset of info included
+    koalaTable = koalaTable.filter((koala, index)=>index !== deleteIndex )
+    // Always send back a response
+    res.sendStatus(200)
+  })
 
-module.exports = koalaRouter;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,57 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
-const koalaRouter = require('./routes/koala.router');
+const koalaRouter = require('./routes/koala.router.js');
 
 app.use(express.json());
-app.use(express.static('server/public'));
-
 // ROUTES
 app.use('/koalas', koalaRouter);
-let i = 0
-let koalaTable = []
-
-// POST request save user input
-app.post('/koalas',(req,res) => {
-  console.log('Post request made for /quotes')
-  //any data we send from the client is available 
-  //as a property of req.body
-  //console.log(req.body)
-  let koalaToAdd = req.body
-  //console.log(koalaToAdd[0].name,'test for name ')
-  koalaTable.push({
-    id:Number(i)+1,
-    name:koalaToAdd[0].name,
-    gender:koalaToAdd[0].gender,
-    age:koalaToAdd[0].age,
-    readyToTransfer:koalaToAdd[0].readyToTransfer,
-    notes:koalaToAdd[0].notes,
-    })
-  i++
-//   for(let j in koalaTable){
-//     koalaTable[j].id++
-// }
-
-  console.log(koalaTable, 'the table is this')
-  res.sendStatus(201)//success
-})
-
-app.get('/koalas',(req,res) => {
-  console.log('GET request made for /quotes')
-  //to send back the list of quotes
-  console.log(koalaTable, 'the get is here ')
-  res.send(koalaTable)
-})
+app.use(express.static('server/public'));
 
 
-app.delete('/koalas/:id',(req,res)=>{
-  //What are we deleting ???
-  console.log(req.params.id)
-  const deleteIndex = Number(req.params.id)  //Similar to req.body but subset of info included
-  koalaTable = koalaTable.filter((koala, index)=>index !== deleteIndex )
-  // Always send back a response
-  res.sendStatus(200)
-})
+
+
+
+
+
+
+
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
