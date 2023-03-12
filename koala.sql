aea9fa3053045d1be2981d2001111a621fac0cf3
Mitch Ludkey <mludk92@hotmail.com>
2023-03-12 10:47:04 -0500

    updated sql

--- koala.sql
+++ koala.sql
@@ -7,7 +7,7 @@ age int not null,
 gender varchar(1) not null,
 ready_to_transfer boolean not null,
 notes varchar(1028),
-transfered varchar(10) default null
+transfered boolean default null
 );
 
 --select * from koala;
